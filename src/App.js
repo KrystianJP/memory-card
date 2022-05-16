76fd919aac46366654165639936908fe90ed6d52
krystianjp <krystianjp97@gmail.com>
2022-05-16 18:01:30 +0100

    added score to header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Game from "./components/Game.js";
+import Score from "./components/Score";
 import "./app.css";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <div className="header">
         <h1>Memory Card Game</h1>
+        <Score score={0} />
       </div>
       <Game />
     </div>
