d8dd363331698307724d2cd6e971a62bec3baf89
krystianjp <krystianjp97@gmail.com>
2022-05-16 17:53:02 +0100

    created header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
+import "./app.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="header">
+        <h1>Memory Card Game</h1>
+      </div>
+    </div>
+  );
 }
 
 export default App;
