b04e9b55d35282e0dde86f02664094b75912865b
krystianjp <krystianjp97@gmail.com>
2022-05-16 17:25:25 +0100

    created cards

---
+++ src/components/Game.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import Card from "Card.js";
+
+function Game(props) {
+  const images = ["", "", "", "", "", "", "", "", "", "", "", ""];
+  const [cards, setCards] = useState(getCards());
+
+  function getCards() {
+    let cards = [];
+    for (let i = 0; i < 12; i++) {
+      setCards(cards.concat(<Card image={images[i]} />));
+    }
+    return cards;
+  }
+}
+
+export default Game;
