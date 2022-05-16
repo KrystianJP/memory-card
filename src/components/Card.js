2f5b09b6af0dd1961187cdef74ebf3131ba7f0ce
krystianjp <krystianjp97@gmail.com>
2022-05-16 17:19:37 +0100

    created div with image from props

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,5 +1,9 @@
 import React, { useState } from "react";
 
-function Card(props) {}
+function Card(props) {
+  const img = <img src={props.image} alt="memory card"></img>;
+
+  return <div className="card">{img}</div>;
+}
 
 export default Card;
